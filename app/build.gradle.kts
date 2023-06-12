a8883a3ccd93e0640e5b7780cddd90c38cb9bfcf
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-06-12 16:27:25 +0200

    refactor: specify version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.2.0-alpha02")
 
     // UI
     implementation("androidx.compose.material:material-icons-extended:1.4.3")
