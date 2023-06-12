434f0aa0c092ee70afbed50fe27d476e45cb74c3
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-06-12 09:18:07 +0200

    feat: added icons

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
 
+    // UI
+    implementation("androidx.compose.material:material-icons-extended:1.4.3")
+
     // Lifecycle
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
