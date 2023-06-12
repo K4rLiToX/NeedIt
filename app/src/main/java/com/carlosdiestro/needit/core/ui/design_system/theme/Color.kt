2769150bd1e8fecd987958f5158082fbe32e73cb
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-06-12 08:22:07 +0200

    refactor: change colors

---
+++ app/src/main/java/com/carlosdiestro/needit/core/ui/design_system/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.carlosdiestro.needit.core.ui.design_system.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Blue70 = Color(0XFF002849)
+val Blue50 = Color(0XFF007FEB)
+val Blue30 = Color(0XFFBADFFF)
+
+val White = Color.White
+val White20 = Color(0xFFF9F9F9)
+
+val Black = Color.Black
+val Black80 = Color(0XFF101010)
+val Black30 = Color(0XFF747474)
+
+val BlackT50 = Color(0X80000000)
+val WhiteT70 = Color(0xB3FFFFFF)
