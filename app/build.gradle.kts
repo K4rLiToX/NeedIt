43bb48313c13af6aa9c62f7b37cec0df16568639
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-06-15 11:33:40 +0200

    chore: add navigation dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -74,6 +74,10 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
 
+    // Navigation
+    implementation("androidx.navigation:navigation-ui-ktx:2.6.0")
+    implementation("androidx.navigation:navigation-compose:2.6.0")
+
     // Compose Test
     androidTestImplementation(platform("androidx.compose:compose-bom:2022.10.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
