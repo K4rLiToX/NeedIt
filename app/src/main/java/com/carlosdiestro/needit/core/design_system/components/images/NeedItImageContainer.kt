2edf274c570782b2617c34c15f0b91baf6ec3484
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-06-18 12:18:44 +0200

    refactor: remove unused imports

--- app/src/main/java/com/carlosdiestro/needit/core/design_system/components/images/NeedItImageContainer.kt
+++ app/src/main/java/com/carlosdiestro/needit/core/design_system/components/images/NeedItImageContainer.kt
@@ -1,10 +1,8 @@
 package com.carlosdiestro.needit.core.design_system.components.images
 
 import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.background
 import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -12,7 +10,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
--- app/src/main/java/com/carlosdiestro/needit/core/design_system/components/navigation/NeedItTopAppBar.kt
+++ app/src/main/java/com/carlosdiestro/needit/core/design_system/components/navigation/NeedItTopAppBar.kt
@@ -17,8 +17,8 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import com.carlosdiestro.needit.R
-import com.carlosdiestro.needit.core.design_system.components.images.NeedItAvatar
 import com.carlosdiestro.needit.core.design_system.components.buttons.NeedItFilledIconButton
+import com.carlosdiestro.needit.core.design_system.components.images.NeedItAvatar
 import com.carlosdiestro.needit.core.design_system.theme.NeedItTheme
 import com.carlosdiestro.needit.core.design_system.theme.icons
 import com.carlosdiestro.needit.core.design_system.theme.spacing
