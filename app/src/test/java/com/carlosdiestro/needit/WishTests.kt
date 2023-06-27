862a7d42a04b26793b4e8730960df20a14d4247b
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-06-27 07:52:59 +0200

    tests: create corresponding test for use cases

---
+++ app/src/test/java/com/carlosdiestro/needit/WishTests.kt
@@ -0,0 +1,27 @@
+package com.carlosdiestro.needit
+
+import com.carlosdiestro.needit.domain.wishes.GetMyWishesUseCase
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Before
+import org.junit.Test
+
+class WishTests {
+    lateinit var repository: FakeWishRepository
+    lateinit var getMyWishes: GetMyWishesUseCase
+
+    @Before
+    fun setup() {
+        repository = FakeWishRepository()
+        getMyWishes = GetMyWishesUseCase(repository)
+    }
+
+    @Test
+    fun `Getting my wishes`() = runBlocking {
+        val myWishes = repository.wishes.first()
+        assertTrue(myWishes.isNotEmpty())
+    }
+}
