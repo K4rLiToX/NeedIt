c1875daee4c511a62ddee375496212d560be9da5
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-07-13 14:32:42 +0200

    feat: add splashscreen

--- app/src/main/java/com/carlosdiestro/needit/MainActivity.kt
+++ app/src/main/java/com/carlosdiestro/needit/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.carlosdiestro.needit.core.Main
 import com.carlosdiestro.needit.core.design_system.theme.NeedItTheme
 import com.carlosdiestro.needit.core.rememberNeedItAppState
@@ -24,6 +25,7 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             NeedItTheme {
                 // A surface container using the 'background' color from the theme
