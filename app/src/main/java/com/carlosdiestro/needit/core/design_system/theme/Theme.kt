c405c7134f8fc0a42910bc748f9c0ea55e514fda
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-08-27 11:57:52 +0200

    refactor: match navigation bar color with bottom nav

--- app/src/main/java/com/carlosdiestro/needit/core/design_system/theme/Theme.kt
+++ app/src/main/java/com/carlosdiestro/needit/core/design_system/theme/Theme.kt
@@ -5,11 +5,13 @@ import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.surfaceColorAtElevation
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalView
+import androidx.compose.ui.unit.dp
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
@@ -87,7 +89,7 @@ fun NeedItTheme(
         SideEffect {
             val window = (view.context as Activity).window
             window.statusBarColor = Color.Transparent.toArgb()
-            window.navigationBarColor = colorScheme.surface.toArgb()
+            window.navigationBarColor = colorScheme.surfaceColorAtElevation(3.dp).toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars =
                 !isDarkTheme
         }
