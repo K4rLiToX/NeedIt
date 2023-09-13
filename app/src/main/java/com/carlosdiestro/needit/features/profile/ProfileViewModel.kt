4142163da07adde70f14316269c69165d61ad93c
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-09-13 08:52:17 +0200

    feat: user profile

--- app/src/main/java/com/carlosdiestro/needit/features/profile/ProfileViewModel.kt
+++ app/src/main/java/com/carlosdiestro/needit/features/profile/ProfileViewModel.kt
@@ -2,19 +2,40 @@ package com.carlosdiestro.needit.features.profile
 
 import androidx.lifecycle.ViewModel
 import com.carlosdiestro.needit.core.design_system.components.cards.SimpleWishPLO
+import com.carlosdiestro.needit.core.extensions.launchCollect
+import com.carlosdiestro.needit.domain.users.User
+import com.carlosdiestro.needit.domain.users.usecases.GetUserInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor() : ViewModel() {
+class ProfileViewModel @Inject constructor(
+    private val getUserInfo: GetUserInfoUseCase
+) : ViewModel() {
     private var _state: MutableStateFlow<ProfileState> = MutableStateFlow(ProfileState())
     val state = _state.asStateFlow()
+
+    init {
+        fetchUserInfo()
+    }
+
+    private fun fetchUserInfo() {
+        launchCollect(getUserInfo()) { user ->
+            _state.update {
+                it.copy(
+                    userInfo = user
+                )
+            }
+        }
+    }
 }
 
 data class ProfileState(
-    val wishes: List<SimpleWishPLO> = emptyList()
+    val wishes: List<SimpleWishPLO> = emptyList(),
+    val userInfo: User? = null
 ) {
     val showEmptyScreen: Boolean = wishes.isEmpty()
 }
