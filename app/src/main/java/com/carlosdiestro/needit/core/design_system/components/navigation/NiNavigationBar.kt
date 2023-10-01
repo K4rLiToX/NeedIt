f56791834a8478443f612790f744e555dd8c518c
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-10-01 11:40:59 +0200

    refactor: combine components

--- app/src/main/java/com/carlosdiestro/needit/core/design_system/components/navigation/NiNavigationBar.kt
+++ app/src/main/java/com/carlosdiestro/needit/core/design_system/components/navigation/NiNavigationBar.kt
@@ -1,8 +1,11 @@
 package com.carlosdiestro.needit.core.design_system.components.navigation
 
 import android.content.res.Configuration
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.NavigationBarItemDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -49,6 +52,33 @@ fun NiNavigationBar(
             )
         }
     }
+}
+
+@Composable
+internal fun RowScope.NiNavigationBarItem(
+    selected: Boolean,
+    onClick: () -> Unit,
+    icon: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+    selectedIcon: @Composable () -> Unit,
+    label: @Composable () -> Unit,
+    alwaysShowLabel: Boolean = true
+) {
+    NavigationBarItem(
+        selected = selected,
+        onClick = onClick,
+        icon = if (selected) selectedIcon else icon,
+        modifier = modifier,
+        label = label,
+        alwaysShowLabel = alwaysShowLabel,
+        colors = NavigationBarItemDefaults.colors(
+            selectedIconColor = NiNavigationSpecs.Color.SelectedIcon,
+            unselectedIconColor = NiNavigationSpecs.Color.UnselectedIcon,
+            selectedTextColor = NiNavigationSpecs.Color.SelectedLabel,
+            unselectedTextColor = NiNavigationSpecs.Color.UnselectedLabel,
+            indicatorColor = NiNavigationSpecs.Color.IndicatorColor
+        )
+    )
 }
 
 private fun NavDestination?.isTopLevelDestinationInHierarchy(destination: TopLevelDestination):
