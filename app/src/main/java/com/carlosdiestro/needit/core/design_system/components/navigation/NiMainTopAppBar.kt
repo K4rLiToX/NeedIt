7b78eeacb230603558c2c2c8084bd048bbad6a23
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-10-01 13:36:26 +0200

    refactor: update top app bar

---
+++ app/src/main/java/com/carlosdiestro/needit/core/design_system/components/navigation/NiMainTopAppBar.kt
@@ -0,0 +1,78 @@
+package com.carlosdiestro.needit.core.design_system.components.navigation
+
+import android.content.res.Configuration
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarState
+import androidx.compose.material3.rememberTopAppBarState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.carlosdiestro.needit.core.design_system.components.avatars.NiAvatar
+import com.carlosdiestro.needit.core.design_system.components.buttons.NiIconButton
+import com.carlosdiestro.needit.core.design_system.components.buttons.NiIconButtonSpecs
+import com.carlosdiestro.needit.core.design_system.theme.NeedItTheme
+import com.carlosdiestro.needit.core.design_system.theme.icons
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NiMainTopAppBar(
+    modifier: Modifier = Modifier,
+    onNotificationClick: () -> Unit,
+    colors: TopAppBarColors = NiTopAppBarSpecs.Color.transparent(),
+    accountImageUrl: String? = null,
+    onAccountClick: () -> Unit,
+    topAppBarState: TopAppBarState = rememberTopAppBarState()
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
+    CenterAlignedTopAppBar(
+        title = {
+            // TODO(Need It Logo)
+        },
+        navigationIcon = {
+            NiIconButton(
+                icon = MaterialTheme.icons.Notification,
+                colors = NiIconButtonSpecs.Color.transparent(),
+                onClick = onNotificationClick
+            )
+        },
+        colors = colors,
+        actions = {
+            NiAvatar(
+                imageUrl = accountImageUrl,
+                onClick = onAccountClick
+            )
+        },
+        modifier = modifier,
+        scrollBehavior = scrollBehavior
+    )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+internal fun NiMainTopAppBarPreview() {
+    NeedItTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
+            NiMainTopAppBar(
+                onNotificationClick = {},
+                accountImageUrl = "https://lh3.googleusercontent" +
+                        ".com/a/ACg8ocIAt4DgDPUFisU08VoLLXtS06lloC8ftW_GpGN1AkekwWY=s96-c",
+                onAccountClick = {}
+            )
+            NiMainTopAppBar(
+                onNotificationClick = {},
+                onAccountClick = {}
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/carlosdiestro/needit/core/design_system/components/navigation/NiTopAppBarSpecs.kt
@@ -0,0 +1,35 @@
+package com.carlosdiestro.needit.core.design_system.components.navigation
+
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.surfaceColorAtElevation
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Immutable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Immutable
+object NiTopAppBarSpecs {
+    @Immutable
+    object Color {
+        @OptIn(ExperimentalMaterial3Api::class)
+        @Composable
+        fun transparent(): TopAppBarColors {
+            return TopAppBarDefaults.topAppBarColors(
+                containerColor = androidx.compose.ui.graphics.Color.Transparent,
+                titleContentColor = MaterialTheme.colorScheme.onSurface,
+                scrolledContainerColor = MaterialTheme.colorScheme.surfaceColorAtElevation(2.dp)
+            )
+        }
+
+        @OptIn(ExperimentalMaterial3Api::class)
+        @Composable
+        fun neutral(): TopAppBarColors {
+            return TopAppBarDefaults.topAppBarColors(
+                containerColor = Color(0x80000000)
+            )
+        }
+    }
+}
