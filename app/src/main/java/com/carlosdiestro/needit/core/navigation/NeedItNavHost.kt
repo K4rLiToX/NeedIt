dec27d45b12f04c8b01a2c8ca2b62f69424106b9
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-10-14 12:03:06 +0200

    refactor: reformat code

--- app/src/main/java/com/carlosdiestro/needit/core/navigation/NeedItNavHost.kt
+++ app/src/main/java/com/carlosdiestro/needit/core/navigation/NeedItNavHost.kt
@@ -4,6 +4,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import com.carlosdiestro.needit.core.NeedItAppState
+import com.carlosdiestro.needit.features.account.accountScreen
 import com.carlosdiestro.needit.features.camera.cameraRoute
 import com.carlosdiestro.needit.features.friends.friendsScreen
 import com.carlosdiestro.needit.features.gifts.giftsScreen
@@ -11,7 +12,6 @@ import com.carlosdiestro.needit.features.home.homeRoute
 import com.carlosdiestro.needit.features.home.homeScreen
 import com.carlosdiestro.needit.features.home.navigateToHome
 import com.carlosdiestro.needit.features.home.navigateToHomeCleaningBackStack
-import com.carlosdiestro.needit.features.account.accountScreen
 import com.carlosdiestro.needit.features.sign_in.signInRoute
 import com.carlosdiestro.needit.features.sign_in.signInScreen
 import com.carlosdiestro.needit.features.upsert_item.navigateToUpsert
