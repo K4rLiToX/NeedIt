a76740c084faa2e28356180b6b799406aa5b2a54
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-10-14 12:10:56 +0200

    refactor: remove unused import

--- app/src/main/java/com/carlosdiestro/needit/features/camera/CameraNavigation.kt
+++ app/src/main/java/com/carlosdiestro/needit/features/camera/CameraNavigation.kt
@@ -3,7 +3,6 @@ package com.carlosdiestro.needit.features.camera
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import kotlinx.coroutines.CoroutineScope
 
 const val cameraRoute = "camera"
 
