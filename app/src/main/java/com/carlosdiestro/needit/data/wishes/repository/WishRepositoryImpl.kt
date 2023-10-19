7982d2f8df93ca1042fcb767601bf37a8c4b96fc
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-10-19 07:30:50 +0200

    refactor: use app scope to allow task to be completed with out cancelling

--- app/src/main/java/com/carlosdiestro/needit/data/wishes/repository/WishRepositoryImpl.kt
+++ app/src/main/java/com/carlosdiestro/needit/data/wishes/repository/WishRepositoryImpl.kt
@@ -1,13 +1,11 @@
 package com.carlosdiestro.needit.data.wishes.repository
 
-import com.carlosdiestro.needit.core.di.ApplicationScope
 import com.carlosdiestro.needit.core.di.IoDispatcher
 import com.carlosdiestro.needit.data.wishes.datasources.WishLocalDatasource
 import com.carlosdiestro.needit.data.wishes.datasources.WishRemoteDatasource
 import com.carlosdiestro.needit.domain.wishes.Wish
 import com.carlosdiestro.needit.domain.wishes.repository.WishRepository
 import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOn
@@ -17,8 +15,7 @@ import javax.inject.Inject
 class WishRepositoryImpl @Inject constructor(
     private val localDatasource: WishLocalDatasource,
     private val remoteDatasource: WishRemoteDatasource,
-    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
-    @ApplicationScope private val appDispatcher: CoroutineScope
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher
 ) : WishRepository {
     override val wishes: Flow<List<Wish>>
         get() = localDatasource.wishes.flowOn(ioDispatcher)
@@ -29,12 +26,11 @@ class WishRepositoryImpl @Inject constructor(
     override fun getWish(id: Long): Flow<Wish> =
         localDatasource.getWish(id).flowOn(ioDispatcher)
 
-    override suspend fun insertWish(wish: Wish): Long =
-        withContext(appDispatcher.coroutineContext) {
-            localDatasource.insertWish(wish)
-        }
+    override suspend fun insertWish(wish: Wish): Long = withContext(ioDispatcher) {
+        localDatasource.insertWish(wish)
+    }
 
-    override suspend fun updateWish(wish: Wish) = withContext(appDispatcher.coroutineContext) {
+    override suspend fun updateWish(wish: Wish) = withContext(ioDispatcher) {
         localDatasource.updateWish(wish)
         if (wish.isShared) remoteDatasource.update(wish)
     }
