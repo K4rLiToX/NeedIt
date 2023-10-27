94b59ff01efe930dfe72abc36c7addd84a207f22
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-10-27 18:10:35 +0200

    feat: include account bottom sheet state

--- app/src/main/java/com/carlosdiestro/needit/core/NeedItApp.kt
+++ app/src/main/java/com/carlosdiestro/needit/core/NeedItApp.kt
@@ -6,6 +6,8 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
@@ -142,26 +144,31 @@ fun Main(
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun rememberNeedItAppState(
     navController: NavHostController = rememberNavController(),
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    accountBottomSheetState: SheetState = rememberModalBottomSheetState()
 ): NeedItAppState {
     return remember(
         navController,
-        coroutineScope
+        coroutineScope,
+        accountBottomSheetState
     ) {
         NeedItAppState(
             navController = navController,
-            coroutineScope = coroutineScope
+            coroutineScope = coroutineScope,
+            accountBottomSheetState = accountBottomSheetState
         )
     }
 }
 
 @Stable
-class NeedItAppState(
+class NeedItAppState @OptIn(ExperimentalMaterial3Api::class) constructor(
     val navController: NavHostController,
-    val coroutineScope: CoroutineScope
+    val coroutineScope: CoroutineScope,
+    val accountBottomSheetState: SheetState
 ) {
     private val routesWithoutStatusBarPadding: List<String> = listOf(
         detailsRoute,
@@ -205,6 +212,9 @@ class NeedItAppState(
     val shouldShowTopBar: Boolean
         @Composable get() = isTopLevelDestination()
 
+    var shouldShowAccountBottomSheet by mutableStateOf(false)
+        private set
+
     fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
         val topLevelNavOptions = navOptions {
             popUpTo(navController.graph.findStartDestination().id) {
@@ -237,4 +247,8 @@ class NeedItAppState(
     @Composable
     private fun isTopLevelDestination(): Boolean =
         currentDestinationRoute in topLevelDestinationsRoutes
+
+    fun setShowAccountBottomSheet(shouldShow: Boolean) {
+        shouldShowAccountBottomSheet = shouldShow
+    }
 }
