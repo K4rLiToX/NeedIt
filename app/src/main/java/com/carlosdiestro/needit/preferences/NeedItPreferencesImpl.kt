b2c6ad7e385ab167a0670d5d5bf2c1af62a19868
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-05 20:45:57 +0100

    refactor: improve readability

--- app/src/main/java/com/carlosdiestro/needit/preferences/NeedItPreferencesImpl.kt
+++ app/src/main/java/com/carlosdiestro/needit/preferences/NeedItPreferencesImpl.kt
@@ -23,67 +23,131 @@ class NeedItPreferencesImpl @Inject constructor(
 ) : NeedItPreferences {
 
     private val preferences = context.preferences
-    private val prefsData = preferences.data
 
-    override val user: Flow<UserPreferences> = prefsData.map { prefs ->
-        UserPreferences(
-            id = prefs[userIdKey].orEmpty(),
-            username = prefs[usernameKey].orEmpty(),
-            email = prefs[userEmailKey].orEmpty(),
-            profilePictureUrl = prefs[userProfilePictureKey].orEmpty(),
-            isAnonymous = prefs[userIsAnonymousKey] ?: true
-        )
-    }
-    override val settings: Flow<SettingsPreferences> = prefsData.map { prefs ->
-        SettingsPreferences(
-            useSystemScheme = prefs[settingsUseSystemSchemeKey] ?: true,
-            isNightMode = prefs[settingsIsNightModeKey] ?: false
-        )
-    }
+    override val user: Flow<UserPreferences> = preferences.user
+
+    override val settings: Flow<SettingsPreferences> = preferences.settings
 
     override suspend fun updateUserInfo(userPreferences: UserPreferences) {
         preferences.edit { prefs ->
-            prefs[userIdKey] = userPreferences.id
-            prefs[usernameKey] = userPreferences.username
-            prefs[userEmailKey] = userPreferences.email
-            prefs[userProfilePictureKey] = userPreferences.profilePictureUrl
-            prefs[userIsAnonymousKey] = userPreferences.isAnonymous
+            prefs.user = userPreferences
         }
     }
 
     override suspend fun updateUseSystemScheme() {
         preferences.edit { prefs ->
-            val value = prefs[settingsUseSystemSchemeKey] ?: false
-            prefs[settingsUseSystemSchemeKey] = !value
+            prefs.useSystemScheme = !prefs.useSystemScheme
         }
     }
 
     override suspend fun updateIsNightMode() {
         preferences.edit { prefs ->
-            val value = prefs[settingsIsNightModeKey] ?: true
-            prefs[settingsIsNightModeKey] = !value
+            prefs.isNightMode = !prefs.isNightMode
         }
     }
 
     override suspend fun cleanPreferences() {
         preferences.edit { prefs -> prefs.clear() }
     }
+}
 
-    companion object {
-        private const val userIdKeyName = "user_id"
-        private const val usernameKeyName = "user_username"
-        private const val userEmailKeyName = "user_email"
-        private const val userProfilePictureUrlKeyName = "user_profile_picture_url"
-        private const val userIsAnonymousKeyName = "user_is_anonymous"
-        private const val settingsUseSystemSchemeKeyName = "settings_use_system_scheme"
-        private const val settingsIsNightModeKeyName = "settings_is_night_mode"
-        private val userIdKey = stringPreferencesKey(userIdKeyName)
-        private val usernameKey = stringPreferencesKey(usernameKeyName)
-        private val userEmailKey = stringPreferencesKey(userEmailKeyName)
-        private val userProfilePictureKey = stringPreferencesKey(userProfilePictureUrlKeyName)
-        private val userIsAnonymousKey = booleanPreferencesKey(userIsAnonymousKeyName)
-        private val settingsUseSystemSchemeKey =
-            booleanPreferencesKey(settingsUseSystemSchemeKeyName)
-        private val settingsIsNightModeKey = booleanPreferencesKey(settingsIsNightModeKeyName)
+internal object UserKeys {
+    internal val id = stringPreferencesKey("user_id")
+    internal val username = stringPreferencesKey("user_username")
+    internal val email = stringPreferencesKey("user_email")
+    internal val profilePictureUrl = stringPreferencesKey("user_profile_picture_url")
+    internal val isAnonymous = booleanPreferencesKey("user_is_anonymous")
+}
+
+internal object SettingsKeys {
+    internal val useSystemScheme = booleanPreferencesKey("settings_use_system_scheme")
+    internal val isNightMode = booleanPreferencesKey("settings_is_night_mode")
+}
+
+internal val DataStore<Preferences>.user: Flow<UserPreferences>
+    get() = this.data.user
+
+internal val DataStore<Preferences>.settings: Flow<SettingsPreferences>
+    get() = this.data.settings
+
+internal val Flow<Preferences>.user: Flow<UserPreferences>
+    get() = this.map { it.user }
+
+internal val Flow<Preferences>.settings: Flow<SettingsPreferences>
+    get() = this.map { it.settings }
+
+internal var Preferences.user: UserPreferences
+    get() = UserPreferences(
+        id = userId,
+        username = username,
+        email = email,
+        profilePictureUrl = profilePictureUrl,
+        isAnonymous = isAnonymous
+    )
+    set(value) {
+        this.userId = value.id
+        this.username = value.username
+        this.email = value.email
+        this.profilePictureUrl = value.profilePictureUrl
+        this.isAnonymous = value.isAnonymous
     }
+
+internal var Preferences.settings: SettingsPreferences
+    get() = SettingsPreferences(
+        useSystemScheme = useSystemScheme,
+        isNightMode = isNightMode
+    )
+    set(value) {
+        this.useSystemScheme = value.useSystemScheme
+        this.isNightMode = value.isNightMode
+    }
+
+internal var Preferences.userId: String
+    get() = this[UserKeys.id].orEmpty()
+    set(value) {
+        this[UserKeys.id] = value
+    }
+
+internal var Preferences.username: String
+    get() = this[UserKeys.username].orEmpty()
+    set(value) {
+        this[UserKeys.username] = value
+    }
+
+internal var Preferences.email: String
+    get() = this[UserKeys.email].orEmpty()
+    set(value) {
+        this[UserKeys.email] = value
+    }
+
+internal var Preferences.profilePictureUrl: String
+    get() = this[UserKeys.profilePictureUrl].orEmpty()
+    set(value) {
+        this[UserKeys.profilePictureUrl] = value
+    }
+
+internal var Preferences.isAnonymous: Boolean
+    get() = this[UserKeys.isAnonymous] ?: true
+    set(value) {
+        this[UserKeys.isAnonymous] = value
+    }
+
+internal var Preferences.useSystemScheme: Boolean
+    get() = this[SettingsKeys.useSystemScheme] ?: true
+    set(value) {
+        this[SettingsKeys.useSystemScheme] = value
+    }
+
+internal var Preferences.isNightMode: Boolean
+    get() = this[SettingsKeys.isNightMode] ?: false
+    set(value) {
+        this[SettingsKeys.isNightMode] = value
+    }
+
+private operator fun Preferences.set(key: Preferences.Key<String>, value: String) {
+    this[key] = value
+}
+
+private operator fun Preferences.set(key: Preferences.Key<Boolean>, value: Boolean) {
+    this[key] = value
 }
