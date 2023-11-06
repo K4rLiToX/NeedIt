854b9e3142915ea8a161ff2802aa9f2af86380e5
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-06 08:41:40 +0100

    refactor: update gifts package visibility

--- app/src/main/kotlin/com/carlosdiestro/needit/features/gifts/GiftsViewModel.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/features/gifts/GiftsViewModel.kt
@@ -7,12 +7,12 @@ import kotlinx.coroutines.flow.asStateFlow
 import javax.inject.Inject
 
 @HiltViewModel
-class GiftsViewModel @Inject constructor() : ViewModel() {
+internal class GiftsViewModel @Inject constructor() : ViewModel() {
     private var _state: MutableStateFlow<GiftsState> = MutableStateFlow(GiftsState())
     val state = _state.asStateFlow()
 }
 
-data class GiftsState(
+internal data class GiftsState(
     val gifts: List<String> = emptyList()
 ) {
     val showEmptyScreen: Boolean = gifts.isEmpty()
