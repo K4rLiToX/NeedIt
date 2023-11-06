31cb02a80e1d2914f678cb95fae87d0882b496f0
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-06 16:45:40 +0100

    refactor: extract to different file

--- app/src/main/kotlin/com/carlosdiestro/needit/core/NeedItApp.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/NeedItApp.kt
@@ -1,6 +1,5 @@
 package com.carlosdiestro.needit.core
 
-import android.content.Context
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -8,21 +7,9 @@ import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavDestination
-import androidx.navigation.NavGraph.Companion.findStartDestination
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.currentBackStackEntryAsState
-import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navOptions
 import com.carlosdiestro.needit.MainViewModel
 import com.carlosdiestro.needit.core.design_system.components.extensions.conditional
 import com.carlosdiestro.needit.core.design_system.components.fab.NiFab
@@ -30,19 +17,12 @@ import com.carlosdiestro.needit.core.design_system.components.menus.CameraPermis
 import com.carlosdiestro.needit.core.design_system.components.menus.PermissionDialog
 import com.carlosdiestro.needit.core.design_system.components.navigation.navigation_bar.NiNavigationBar
 import com.carlosdiestro.needit.core.design_system.components.navigation.navigation_bar.TopLevelDestination
-import com.carlosdiestro.needit.core.design_system.components.navigation.navigation_bar.routes
 import com.carlosdiestro.needit.core.design_system.components.navigation.top_app_bar.NiMainTopAppBar
 import com.carlosdiestro.needit.core.design_system.theme.icons
 import com.carlosdiestro.needit.core.navigation.NeedItNavHost
 import com.carlosdiestro.needit.features.account.AccountDialogRoute
-import com.carlosdiestro.needit.features.camera.cameraRoute
-import com.carlosdiestro.needit.features.home.navigateToHome
 import com.carlosdiestro.needit.features.settings.navigateToSettings
-import com.carlosdiestro.needit.features.settings.settingsRoute
 import com.carlosdiestro.needit.features.sign_in.navigateToSignIn
-import com.carlosdiestro.needit.features.upsert_item.upsertRoute
-import com.carlosdiestro.needit.features.wish_details.detailsRoute
-import kotlinx.coroutines.CoroutineScope
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -158,118 +138,5 @@ fun NeedItApp(
                 appState.navController.navigateToSettings()
             }
         )
-    }
-}
-
-@Composable
-fun rememberNeedItAppState(
-    navController: NavHostController = rememberNavController(),
-    coroutineScope: CoroutineScope = rememberCoroutineScope(),
-    context: Context = LocalContext.current
-): NeedItAppState {
-    return remember(
-        navController,
-        coroutineScope,
-        context
-    ) {
-        NeedItAppState(
-            navController = navController,
-            coroutineScope = coroutineScope,
-            context = context
-        )
-    }
-}
-
-@Stable
-class NeedItAppState constructor(
-    val navController: NavHostController,
-    val coroutineScope: CoroutineScope,
-    val context: Context
-) {
-    private val routesWithoutStatusBarPadding: List<String> = listOf(
-        detailsRoute,
-        cameraRoute,
-        upsertRoute,
-        settingsRoute
-    )
-
-    private val routesWithoutNavigationBarPadding: List<String> = listOf(
-        detailsRoute
-    )
-
-    val currentDestination: NavDestination?
-        @Composable get() = navController
-            .currentBackStackEntryAsState().value?.destination
-
-    val currentDestinationRoute: String
-        @Composable get() = currentDestination?.route ?: ""
-
-    val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
-    private val topLevelDestinationsWithFab: List<String> = listOf(
-        TopLevelDestination.Home.route,
-        TopLevelDestination.Friends.route
-    )
-    private val topLevelDestinationsRoutes: List<String> = topLevelDestinations.routes()
-
-    val shouldShowFab: Boolean
-        @Composable get() = currentDestinationRoute in topLevelDestinationsWithFab
-
-    val shouldShowBottomBar: Boolean
-        @Composable get() = isTopLevelDestination()
-
-    var shouldShowCameraPermissionDialog by mutableStateOf(false)
-        private set
-
-    val shouldNotHaveStatusBarPadding: Boolean
-        @Composable get() = currentDestinationRoute in routesWithoutStatusBarPadding
-
-    val shouldNotHaveNavigationBarPadding: Boolean
-        @Composable get() = currentDestinationRoute in routesWithoutNavigationBarPadding
-
-    val shouldShowTopBar: Boolean
-        @Composable get() = isTopLevelDestination()
-
-    var shouldShowAccountDialog by mutableStateOf(false)
-        private set
-
-    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
-        val topLevelNavOptions = navOptions {
-            popUpTo(navController.graph.findStartDestination().id) {
-                saveState = true
-            }
-            launchSingleTop = true
-            restoreState = true
-        }
-        when (topLevelDestination) {
-            TopLevelDestination.Home -> navController.navigateToHome(
-                topLevelNavOptions
-            )
-
-            TopLevelDestination.Gifts -> navController.navigate(
-                topLevelDestination.route,
-                topLevelNavOptions
-            )
-
-            TopLevelDestination.Friends -> navController.navigate(
-                topLevelDestination.route,
-                topLevelNavOptions
-            )
-        }
-    }
-
-    fun setShowCameraPermissionDialog(shouldShow: Boolean) {
-        shouldShowCameraPermissionDialog = shouldShow
-    }
-
-    @Composable
-    private fun isTopLevelDestination(): Boolean =
-        currentDestinationRoute in topLevelDestinationsRoutes
-
-    fun closeAccountDialog() {
-        shouldShowAccountDialog = false
-    }
-
-    fun openAccountDialog() {
-        shouldShowAccountDialog = true
     }
 }
---
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/NeedItAppState.kt
@@ -0,0 +1,138 @@
+package com.carlosdiestro.needit.core
+
+import android.content.Context
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavDestination
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navOptions
+import com.carlosdiestro.needit.core.design_system.components.navigation.navigation_bar.TopLevelDestination
+import com.carlosdiestro.needit.core.design_system.components.navigation.navigation_bar.routes
+import com.carlosdiestro.needit.features.camera.cameraRoute
+import com.carlosdiestro.needit.features.home.navigateToHome
+import com.carlosdiestro.needit.features.settings.settingsRoute
+import com.carlosdiestro.needit.features.upsert_item.upsertRoute
+import com.carlosdiestro.needit.features.wish_details.detailsRoute
+import kotlinx.coroutines.CoroutineScope
+
+@Composable
+fun rememberNeedItAppState(
+    navController: NavHostController = rememberNavController(),
+    coroutineScope: CoroutineScope = rememberCoroutineScope(),
+    context: Context = LocalContext.current
+): NeedItAppState {
+    return remember(
+        navController,
+        coroutineScope,
+        context
+    ) {
+        NeedItAppState(
+            navController = navController,
+            coroutineScope = coroutineScope,
+            context = context
+        )
+    }
+}
+
+@Stable
+class NeedItAppState constructor(
+    val navController: NavHostController,
+    val coroutineScope: CoroutineScope,
+    val context: Context
+) {
+    private val routesWithoutStatusBarPadding: List<String> = listOf(
+        detailsRoute,
+        cameraRoute,
+        upsertRoute,
+        settingsRoute
+    )
+
+    private val routesWithoutNavigationBarPadding: List<String> = listOf(
+        detailsRoute
+    )
+
+    val currentDestination: NavDestination?
+        @Composable get() = navController
+            .currentBackStackEntryAsState().value?.destination
+
+    val currentDestinationRoute: String
+        @Composable get() = currentDestination?.route ?: ""
+
+    val topLevelDestinations: List<TopLevelDestination> = TopLevelDestination.values().asList()
+    private val topLevelDestinationsWithFab: List<String> = listOf(
+        TopLevelDestination.Home.route,
+        TopLevelDestination.Friends.route
+    )
+    private val topLevelDestinationsRoutes: List<String> = topLevelDestinations.routes()
+
+    val shouldShowFab: Boolean
+        @Composable get() = currentDestinationRoute in topLevelDestinationsWithFab
+
+    val shouldShowBottomBar: Boolean
+        @Composable get() = isTopLevelDestination()
+
+    var shouldShowCameraPermissionDialog by mutableStateOf(false)
+        private set
+
+    val shouldNotHaveStatusBarPadding: Boolean
+        @Composable get() = currentDestinationRoute in routesWithoutStatusBarPadding
+
+    val shouldNotHaveNavigationBarPadding: Boolean
+        @Composable get() = currentDestinationRoute in routesWithoutNavigationBarPadding
+
+    val shouldShowTopBar: Boolean
+        @Composable get() = isTopLevelDestination()
+
+    var shouldShowAccountDialog by mutableStateOf(false)
+        private set
+
+    fun navigateToTopLevelDestination(topLevelDestination: TopLevelDestination) {
+        val topLevelNavOptions = navOptions {
+            popUpTo(navController.graph.findStartDestination().id) {
+                saveState = true
+            }
+            launchSingleTop = true
+            restoreState = true
+        }
+        when (topLevelDestination) {
+            TopLevelDestination.Home -> navController.navigateToHome(
+                topLevelNavOptions
+            )
+
+            TopLevelDestination.Gifts -> navController.navigate(
+                topLevelDestination.route,
+                topLevelNavOptions
+            )
+
+            TopLevelDestination.Friends -> navController.navigate(
+                topLevelDestination.route,
+                topLevelNavOptions
+            )
+        }
+    }
+
+    fun setShowCameraPermissionDialog(shouldShow: Boolean) {
+        shouldShowCameraPermissionDialog = shouldShow
+    }
+
+    @Composable
+    private fun isTopLevelDestination(): Boolean =
+        currentDestinationRoute in topLevelDestinationsRoutes
+
+    fun closeAccountDialog() {
+        shouldShowAccountDialog = false
+    }
+
+    fun openAccountDialog() {
+        shouldShowAccountDialog = true
+    }
+}
