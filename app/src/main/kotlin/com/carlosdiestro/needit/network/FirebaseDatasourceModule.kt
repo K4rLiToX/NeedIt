b8587fbc8978988f51d88698115da6ce6cb07222
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-06 08:33:21 +0100

    refactor: rename to distinguish

--- app/src/main/kotlin/com/carlosdiestro/needit/network/FirebaseDatasourceModule.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/network/FirebaseDatasourceModule.kt
@@ -14,7 +14,7 @@ import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-internal interface DatasourceModule {
+internal interface FirebaseDatasourceModule {
 
     @Singleton
     @Binds
