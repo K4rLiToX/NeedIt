ab20a7b57a3f7e0b5f4c9b6b42a47d6f34279479
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-08 16:34:37 +0100

    refactor: remove app scrolling behavior from main top bar

--- app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/navigation/top_app_bar/NiMainTopAppBar.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/navigation/top_app_bar/NiMainTopAppBar.kt
@@ -10,9 +10,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBarState
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -33,10 +30,8 @@ fun NiMainTopAppBar(
     onNotificationClick: () -> Unit,
     colors: TopAppBarColors = NiTopAppBarSpecs.Color.transparent(),
     accountImageUrl: String? = null,
-    onAccountClick: () -> Unit,
-    topAppBarState: TopAppBarState = rememberTopAppBarState()
+    onAccountClick: () -> Unit
 ) {
-    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(topAppBarState)
     CenterAlignedTopAppBar(
         title = {
             Icon(
@@ -62,8 +57,7 @@ fun NiMainTopAppBar(
                 modifier = Modifier.padding(end = MaterialTheme.dimensions.spacingM)
             )
         },
-        modifier = modifier,
-        scrollBehavior = scrollBehavior
+        modifier = modifier
     )
 }
 
