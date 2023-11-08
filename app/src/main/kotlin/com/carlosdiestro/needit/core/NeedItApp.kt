f2807ea5cc4be7b0a922d05f1bd9b4af5dbfa1d2
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-08 16:41:56 +0100

    refactor: remove unused imports

--- app/src/main/kotlin/com/carlosdiestro/needit/core/NeedItApp.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/NeedItApp.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.windowsizeclass.WindowSizeClass
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.carlosdiestro.needit.core.design_system.components.extensions.conditional
 import com.carlosdiestro.needit.core.design_system.components.navigation.navigation_bar.NiNavigationBar
 import com.carlosdiestro.needit.core.design_system.components.navigation.top_app_bar.NiMainTopAppBar
 import com.carlosdiestro.needit.core.navigation.NeedItNavHost
--- app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/navigation/top_app_bar/NiMediumTopAppBar.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/navigation/top_app_bar/NiMediumTopAppBar.kt
@@ -9,10 +9,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.MediumTopAppBar
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarColors
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.TopAppBarScrollBehavior
-import androidx.compose.material3.TopAppBarState
-import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
