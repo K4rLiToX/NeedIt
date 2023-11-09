8466aa15208c17687ad98f671cd978468921fc05
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-09 14:24:29 +0100

    refactor: reformat code

--- app/src/main/kotlin/com/carlosdiestro/needit/data/wishes/repository/WishRepositoryImpl.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/data/wishes/repository/WishRepositoryImpl.kt
@@ -8,7 +8,6 @@ import com.carlosdiestro.needit.data.wishes.datasources.WishRemoteDatasource
 import com.carlosdiestro.needit.domain.wishes.Wish
 import com.carlosdiestro.needit.domain.wishes.repository.WishRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 internal class WishRepositoryImpl @Inject constructor(
@@ -32,8 +31,10 @@ internal class WishRepositoryImpl @Inject constructor(
         when {
             wish.isShared && wish.cloudId.isEmpty() -> remoteDatasource.create(wish.asDto())
             wish.isShared -> remoteDatasource.update(wish.cloudId, wish.asDto())
-            !wish.isShared && wish.cloudId.isNotEmpty() -> remoteDatasource.delete(wish.cloudId, wish
-                .userId)
+            !wish.isShared && wish.cloudId.isNotEmpty() -> remoteDatasource.delete(
+                wish.cloudId,
+                wish.userId
+            )
         }
     }
 
