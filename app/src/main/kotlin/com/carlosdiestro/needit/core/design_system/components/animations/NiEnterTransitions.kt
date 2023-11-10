6566796d0405154ff625f2804ba2dd2a52af5297
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-10 10:45:42 +0100

    refactor: improve top level transition

--- app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/animations/NiEnterTransitions.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/animations/NiEnterTransitions.kt
@@ -2,9 +2,11 @@ package com.carlosdiestro.needit.core.design_system.components.animations
 
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
+import androidx.compose.animation.scaleIn
 import androidx.navigation.NavBackStackEntry
 
 val topLevelEnterTransition: AnimatedContentTransitionScope<NavBackStackEntry>.() ->
@@ -12,7 +14,12 @@ EnterTransition = {
     fadeIn(
         animationSpec = tween(
             durationMillis = 300,
-            easing = LinearEasing
+            easing = FastOutSlowInEasing
+        )
+    ) + scaleIn(
+        animationSpec = tween(
+            durationMillis = 100,
+            easing = FastOutSlowInEasing
         )
     )
 }
--- app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/animations/NiExitTransitions.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/core/design_system/components/animations/NiExitTransitions.kt
@@ -2,17 +2,24 @@ package com.carlosdiestro.needit.core.design_system.components.animations
 
 import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.ExitTransition
+import androidx.compose.animation.core.FastOutSlowInEasing
 import androidx.compose.animation.core.LinearEasing
 import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeOut
+import androidx.compose.animation.scaleOut
 import androidx.navigation.NavBackStackEntry
 
 val topLeveExitTransition: AnimatedContentTransitionScope<NavBackStackEntry>.() ->
 ExitTransition = {
     fadeOut(
         animationSpec = tween(
+            durationMillis = 100,
+            easing = FastOutSlowInEasing
+        )
+    ) + scaleOut(
+        animationSpec = tween(
             durationMillis = 300,
-            easing = LinearEasing
+            easing = FastOutSlowInEasing
         )
     )
 }
