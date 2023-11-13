a7cd8329160e69bbd286469b6daf8cb461e864a1
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-13 13:09:55 +0100

    refactor: improve code

--- app/src/main/kotlin/com/carlosdiestro/needit/device_storage/DeviceStorageModule.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/device_storage/DeviceStorageModule.kt
@@ -2,6 +2,9 @@ package com.carlosdiestro.needit.device_storage
 
 import android.content.ContentResolver
 import android.content.Context
+import android.net.Uri
+import android.os.Build
+import android.provider.MediaStore
 import com.carlosdiestro.needit.core.di.IoDispatcher
 import com.carlosdiestro.needit.data.wishes.datasources.FileManager
 import dagger.Module
@@ -22,8 +25,18 @@ internal object DeviceStorageModule {
 
     @Provides
     @Singleton
+    fun provideImageStorage(): Uri =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            MediaStore.Images.Media.getContentUri(MediaStore.VOLUME_EXTERNAL)
+        } else {
+            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        }
+
+    @Provides
+    @Singleton
     fun provideFileManager(
+        imageStorage: Uri,
         contentResolver: ContentResolver,
         @IoDispatcher ioDispatcher: CoroutineDispatcher
-    ): FileManager = FileManagerImpl(contentResolver, ioDispatcher)
+    ): FileManager = FileManagerImpl(imageStorage, contentResolver, ioDispatcher)
 }
--- app/src/main/kotlin/com/carlosdiestro/needit/device_storage/FileManagerImpl.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/device_storage/FileManagerImpl.kt
@@ -1,8 +1,8 @@
 package com.carlosdiestro.needit.device_storage
 
 import android.content.ContentResolver
+import android.database.Cursor
 import android.net.Uri
-import android.os.Build
 import android.provider.MediaStore
 import com.carlosdiestro.needit.data.wishes.datasources.FileManager
 import kotlinx.coroutines.CoroutineDispatcher
@@ -10,37 +10,32 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 internal class FileManagerImpl @Inject constructor(
+    private val imageStorage: Uri,
     private val contentResolver: ContentResolver,
     private val ioDispatcher: CoroutineDispatcher
 ) : FileManager {
-    private val folder: Uri
-        get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-            MediaStore.Images.Media.getContentUri(
-                MediaStore.VOLUME_EXTERNAL
-            )
-        } else {
-            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-        }
-
-    private val projection: Array<String>
-        get() = arrayOf(
-            MediaStore.Images.Media.DATA
-        )
 
     override suspend fun getImageLocalPath(): String = withContext(ioDispatcher) {
-        contentResolver.query(
-            folder,
-            projection,
-            null,
-            null,
-            null
-        )?.use { cursor ->
-            val dataColumn = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
-            if (cursor.moveToLast()) {
-                cursor.getString(dataColumn)
-            } else {
-                Uri.EMPTY.toString()
-            }
-        } ?: Uri.EMPTY.toString()
+        contentResolver.queryImagePath(imageStorage)
     }
 }
+
+private fun ContentResolver.queryImagePath(imageStorage: Uri): String {
+    return this.query(
+        imageStorage,
+        arrayOf(MediaStore.Images.Media.DATA),
+        null,
+        null,
+        null
+    )?.use { cursor -> cursor.imagePath } ?: Uri.EMPTY.toString()
+}
+
+private val Cursor.imagePath: String
+    get() = if (this.moveToLast()) {
+        this.getString(
+            this.getColumnIndexOrThrow(MediaStore.Images.Media.DATA)
+        )
+    } else {
+        Uri.EMPTY.toString()
+    }
+
