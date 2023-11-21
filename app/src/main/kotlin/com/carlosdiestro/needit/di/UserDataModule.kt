4e642ef00752dcaebd5a7b64d911852f02dc8bbf
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2023-11-21 18:08:03 +0100

    refactor: reformat code

--- app/src/main/kotlin/com/carlosdiestro/needit/di/UserDataModule.kt
+++ app/src/main/kotlin/com/carlosdiestro/needit/di/UserDataModule.kt
@@ -2,10 +2,10 @@ package com.carlosdiestro.needit.di
 
 import com.carlosdiestro.needit.data.theme_config.ThemeConfigRepositoryImpl
 import com.carlosdiestro.needit.data.users.UserRepositoryImpl
-import com.carlosdiestro.wish.data.repository.ImageRepositoryImpl
-import com.carlosdiestro.wish.data.repository.WishRepositoryImpl
 import com.carlosdiestro.needit.domain.theme_config.repository.ThemeConfigRepository
 import com.carlosdiestro.needit.domain.users.repository.UserRepository
+import com.carlosdiestro.wish.data.repository.ImageRepositoryImpl
+import com.carlosdiestro.wish.data.repository.WishRepositoryImpl
 import com.carlosdiestro.wish.domain.repository.ImageRepository
 import com.carlosdiestro.wish.domain.repository.WishRepository
 import dagger.Binds
