820e77da0a661fea996718ccb63908d170e850ae
Carlos Diestro <48551464+K4rLiToX@users.noreply.github.com>
2024-01-13 13:06:05 +0100

    refactor: show friends with wishes and with no wishes

--- feature/friends/src/main/kotlin/com/carlosdiestro/feature/friends/FriendsService.kt
+++ feature/friends/src/main/kotlin/com/carlosdiestro/feature/friends/FriendsService.kt
@@ -5,11 +5,12 @@ import com.carlosdiestro.friend.domain.FriendRepository
 import com.carlosdiestro.user.domain.UserRepository
 import com.carlosdiestro.wish.domain.model.Wish
 import com.carlosdiestro.wish.domain.repository.WishRepository
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.emitAll
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 typealias FriendWithWishes = Pair<Friend, List<Wish>>
@@ -25,25 +26,22 @@ class FriendsService @Inject constructor(
     fun getFriendsWithWishes(): Flow<List<FriendWithWishes>> = flow {
         val friends = friendRepository.getAll().first()
         val ids = friends.map { it.id }
-        val friendWithWishes = wishRepository.getFriendsWishes(ids).map {
-            it.groupBy { wish -> wish.userId }
-        }.map {
-            if (it.isEmpty()) {
-                friends.map { f ->
-                    FriendWithWishes(
-                        f,
-                        emptyList()
-                    )
-                }
-            } else {
-                it.keys.map { id ->
-                    FriendWithWishes(
-                        friends.find { f -> f.id == id }!!,
-                        it[id]!!
-                    )
-                }
-            }
+        val wishesGroupByFriend = wishRepository.getFriendsWishes(ids)
+            .first()
+            .groupByUserId()
+
+        val friendWithWishes = friends.map { friend ->
+            FriendWithWishes(
+                first = friend,
+                second = wishesGroupByFriend.getOrDefault(friend.id, emptyList())
+            )
         }
-        emitAll(friendWithWishes)
+
+        emit(friendWithWishes)
     }
 }
+
+private suspend fun List<Wish>.groupByUserId(): Map<String, List<Wish>> =
+    withContext(Dispatchers.Default) {
+        this@groupByUserId.groupBy { wish -> wish.userId }
+    }
